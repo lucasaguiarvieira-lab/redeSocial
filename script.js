cdeb632392319e3520ed1639dbc7e67bf1841996
lucasaguiarvieira-lab <lucas.aguiar.vieira@escola.pr.gov.br>
2026-08-21 11:07:07 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+    //botao curtida
     const likeBtn = document.qurySelector(".left-actions .action-btn:first-child");
+if (!likeBtn) return;
+
+const likeSvg = likeBtn.querySelector("svg")
+// contador 
+let textNode = Array.from(likeBtn.childNodes).find(
+  (node) => node.nodeType === node. TEXT_NODE && node.textContent. trim()!==)
+
+   //zerando o contador inicial
+   let cont=0;
+   
+   //atualiza
+   (textNode) { textNode.textContent= `0`;}
+
+// coração 
+function applyLikedStyle() {
+  likeSvg.style.fill = "#ef4444";
+  likeSvg.style.stroke = "#ef444";
+  likeBtn.style.fill = 
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
